99a74a4be9bd3ceec71358d1dcd3cc309ad89011
Liam Butterworth <wbbutterworth@gmail.com>
2017-10-20 14:46:54 -0500

    create regex to bust up chord symbols; refactor chord and interval data.json files to be arrays of objects to use with Array.prototype.filter()

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const Chord = require( './source/Chord' );
 
-const chord = new Chord( 'Cmmaj7' );
+const chord = new Chord( 'Csus2' );
+// const chord2 = new Chord( 'Cmaj7' );
+// const chord3 = new Chord( 'C7' );
+// const chord4 = new Chord( 'Cm' );
--- source/Chord/index.js
+++ source/Chord/index.js
@@ -1,43 +1,12 @@
-// const Interval = require( './Interval' );
+const chords = require( './data.json' );
 
-const Chord = function( notation ) {
-	this.notation  = notation
-	this.intervals = [];
+const Chord = function( symbol ) {
+	const match = symbol.match( /(^[A-G])([maj|m|dim|aug]*[7|9|11|13]*)($|sus[2|4]|add[2|4|6]|#4)/ );
+	const root  = match && match[1] ? match[1] : undefined;
+	const abbr  = match && match[2] ? match[2] : 'maj';
+	const mod   = match && match[3] ? match[3] : undefined;
 
-	this.root = notation.match( /^[A-G]/ ).shift();
-	this.quality = notation.match( /(?!^[A-G])(?:maj|m|dim|aug)/ ).shift()
-
-	// switch ( quality ) {
-	// 	case 'maj':
-	// 		this.intervals.concat([
-	// 			new Interval( 'M3' ),
-	// 			new Interval( 'P5' ),
-	// 		]);
-	// 		break;
-
-	// 	case 'm':
-	// 		this.intervals.concat([
-	// 			new Interval( 'm3' ),
-	// 			new Interval( 'P5' ),
-	// 		]);
-	// 		break;
-
-	// 	case 'dim':
-	// 		this.intervals.concat([
-	// 			new Interval( 'm3' ),
-	// 			new Interval( 'dim5' ),
-	// 		]);
-	// 		break;
-
-	// 	case 'aug':
-	// 		this.intervals.concat([
-	// 			new Interval( 'M3' ),
-	// 			new Interval( 'A5' ),
-	// 		]);
-	// 		break;
-	// }
-
-	console.log( 'create chord' );
+	console.log( root, abbr, mod );
 }
 
 module.exports = Chord;
