908cd3b0f7608b327f9b1ea6178f547ae27402e0
Liam Butterworth <liam@Liams-MBP.attlocal.net>
2017-10-24 18:17:33 -0500

    add the chord builder function definition; move regex into variables for reuse in builder function

--- source/Chord/index.js
+++ source/Chord/index.js
@@ -2,22 +2,25 @@
 // Chord
 //
 // :: Constructor
-// :: Notes
+// :: Builder
 
 const data     = require( './data.json' );
 const Note     = require( '../Note' );
 const Interval = require( '../Interval' );
+
+const regexSymbolMatch   = /(^[A-G])\/?([A-G])?((?:mmaj|maj|m|dim|aug)?(?:5|6|7|9|11|13)?)(.*)/;
+const regexModifierSplit = /(?=[#|b]\d*)|(?=add\d*)|(?=sus\d)|(?=no\d)/;
 
 //
 // Constructor
 //
 
 const Chord = function( symbol ) {
-	const symbolMatch    = symbol.match( /(^[A-G])\/?([A-G])?((?:mmaj|maj|m|dim|aug)?(?:5|6|7|9|11|13)?)(.*)/ );
+	const symbolMatch    = symbol.match( regexSymbolMatch );
 	const rootSymbol     = symbolMatch && symbolMatch[1] ? symbolMatch[1] : undefined;
 	const bottomSymbol   = symbolMatch && symbolMatch[2] ? symbolMatch[2] : undefined;
 	const chordSymbol    = symbolMatch && symbolMatch[3] ? symbolMatch[3] : 'maj';
-	const chordModifiers = symbolMatch && symbolMatch[4] ? symbolMatch[4].split( /(?=[#|b]\d*)|(?=add\d*)|(?=sus\d)|(?=no\d)/ ) : undefined;
+	const chordModifiers = symbolMatch && symbolMatch[4] ? symbolMatch[4].split( regexModifierSplit ) : undefined;
 
 	const entry = data.find( ( entry ) => {
 		return entry.symbol === chordSymbol;
@@ -35,6 +38,14 @@ const Chord = function( symbol ) {
 	} );
 
 	this.notes = new Note.Collection.fromIntervals( this.intervals );
+}
+
+//
+// Builder
+//
+
+const Chord.Builder = function() {
+
 }
 
 module.exports = Chord;
