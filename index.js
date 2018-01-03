9e3c12efc05c4029500191c3b8c6b7306040d49b
Liam Butterworth <wbbutterworth@gmail.com>
2018-01-03 13:01:44 -0600

    add output for code sample

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const Chord    = require( './source/Chord' );
 const Scale    = require( './source/Scale' );
 const Key      = require( './source/Key' );
 
+console.log( new Chord( 'Cmaj7' ) );
+
 module.exports = { Note, Interval, Chord, Scale, Key };
